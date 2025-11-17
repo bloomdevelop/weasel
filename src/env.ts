a443b5998cbcca5cbb1083661803300980c75993
Bloom Perez <springdotgay@tuta.io>
2025-11-17 09:46:54 -0400

    refactor: update env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,10 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-    server: {
-        STOAT_BOT_TOKEN: z.string().min(1).nonoptional(),
-        PREFIX: z.string().min(1).nonoptional().default("/")
-    },
-    runtimeEnv: process.env
+  server: {
+    STOAT_BOT_TOKEN: z.string().min(1).nonoptional(),
+    PREFIX: z.string().min(1).default("/"),
+    DB_FILE_NAME: z.string().min(1).nonoptional(),
+  },
+  runtimeEnv: process.env,
 });
